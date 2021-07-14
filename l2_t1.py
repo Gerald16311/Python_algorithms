2ef295edcb730a7cde625cb71c9b9dada31e02b8
Ilia Marchukov <Gerald16311@gmail.com>
2021-07-14 13:53:21 +0400

    1 - 8 done

--- l2_t1.py
+++ l2_t1.py
@@ -4,3 +4,39 @@
 # Если пользователь вводит неверный знак (не '0', '+', '-', '*', '/'), то программа должна сообщать ему об ошибке и
 # снова запрашивать знак операции. Также сообщать пользователю о невозможности деления на ноль, если он ввел 0 в
 # качестве делителя.
+
+first_number = 0
+second_number = 0
+operand = ""
+
+
+def input_numbers():
+    global first_number
+    global second_number
+    first_number = int(input("Введите первое число - "))
+    second_number = int(input("Введите второе число - "))
+
+
+while operand != 0:
+    operand = input("Введите какую операцию вы хотите сделать над числами (0 - выход) - ")
+    if operand == '+':
+        input_numbers()
+        print(f"Сумма - {first_number + second_number}")
+    elif operand == '-':
+        input_numbers()
+        print(f"Разница - {first_number - second_number}")
+    elif operand == '*':
+        input_numbers()
+        print(f"Произведение - {first_number * second_number}")
+    elif operand == '/':
+        input_numbers()
+        if second_number != 0:
+            print(f"Разделение - {first_number / second_number}")
+        else:
+            print('На 0 делить нельзя')
+            continue
+    elif operand == "0":
+        print('Выход')
+        break
+    else:
+        print("Введен не верный символ операции")
