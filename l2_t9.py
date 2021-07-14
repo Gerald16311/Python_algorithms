a272cdf50480df267348b8d5c83bca052d0d3483
Ilia Marchukov <Gerald16311@gmail.com>
2021-07-14 14:59:52 +0400

    9 done

--- l2_t9.py
+++ l2_t9.py
@@ -2,11 +2,24 @@
 # его цифр.
 numbers_list = input("Введите список натуральных чисел, разделенных пробелом - ")
 
-while len(numbers_list) != 0:
-    number = numbers_list[0: numbers_list.index(" ")]
+# while len(numbers_list) != 0:
+#     number = numbers_list[0: (numbers_list.find(" ")) if numbers_list.find(" ") != -1 else (len(numbers_list)-1)]
+#     numbers_sum = 0
+#     for i in number:
+#         numbers_sum += int(i)
+#     numbers_list = numbers_list[numbers_list.find(" ")+1:]
+#     print(f"число выдернутое из списка - {number}, сумма - {numbers_sum}")
+#     print(f"оставшийся список - {numbers_list}")
+
+user_list = numbers_list.split(" ")
+numbers_list_sum = []
+
+for i in user_list:
     numbers_sum = 0
-    for i in number:
-        numbers_sum += int(i)
-    numbers_list = numbers_list[numbers_list.index(" ")+1:]
-    print(f"число выдернутое из списка - {number}, сумма - {numbers_sum}")
-    print(f"оставшийся список - {numbers_list}")
+    for y in i:
+        numbers_sum += int(y)
+    numbers_list_sum.append(numbers_sum)
+
+print(f"Самая большая сумма цифр среди введеных чисел - {user_list[numbers_list_sum.index(max(numbers_list_sum))]}, "
+      f"сумма - {max(numbers_list_sum)}")
+
