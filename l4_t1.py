c157dd07501aea45a7a97a192b94e7eb5bcee7ba
Ilia Marchukov <Gerald16311@gmail.com>
2021-07-29 09:22:54 +0400

    all_done

--- l4_t1.py
+++ l4_t1.py
@@ -15,43 +15,57 @@ def first_result(n):
     return result
 
 
+def main_first_result():
+    for _ in range(1000):
+        first_result(number)
+
+
 print(f"1 Результат (цикл) - {first_result(number)}")
-print(timeit.timeit(first_result(number)))
-cProfile.run('first_result(number)')
+print(timeit.timeit(first_result(number), number=1000))
+cProfile.run('main_first_result()')
+
 
 # 1 Результат (цикл) - 987654321
-# 0.0058531
-#          5 function calls in 0.000 seconds
+# 5.700000000000843e-06
+#          2004 function calls in 0.001 seconds
 #
 #    Ordered by: standard name
 #
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
-#         1    0.000    0.000    0.000    0.000 l4_t1.py:11(first_result)
-#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
-#         1    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#         1    0.000    0.000    0.001    0.001 <string>:1(<module>)
+#      1000    0.001    0.000    0.001    0.000 l4_t1.py:11(first_result)
+#         1    0.000    0.000    0.001    0.001 l4_t1.py:18(main_first_result)
+#         1    0.000    0.000    0.001    0.001 {built-in method builtins.exec}
+#      1000    0.000    0.000    0.000    0.000 {built-in method builtins.len}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
 
 # 2 вариант через срез
 def slised(n):
-    result = number[::-1]
+    result = n[::-1]
     return result
 
 
+def main_slised():
+    for _ in range(1000):
+        slised(number)
+
+
 print(f"2 Результат (срез) - {slised(number)}")
-print(timeit.timeit(slised(number)))
-cProfile.run('slised(number)')
+print(timeit.timeit(slised(number), number=1000))
+cProfile.run('main_slised()')
+
 
 # 2 Результат (срез) - 987654321
-# 0.006168800000000002
-#          4 function calls in 0.000 seconds
+# 8.30000000000275e-06
+#          1004 function calls in 0.000 seconds
 #
 #    Ordered by: standard name
 #
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
 #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
-#         1    0.000    0.000    0.000    0.000 l4_t1.py:24(slised)
+#      1000    0.000    0.000    0.000    0.000 l4_t1.py:29(slised)
+#         1    0.000    0.000    0.000    0.000 l4_t1.py:34(main_slised)
 #         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
@@ -64,19 +78,26 @@ def recursion(n):
         return str(n % 10) + str(recursion(n // 10))
 
 
+def main_recursion():
+    for _ in range(1000):
+        recursion(int(number))
+
+
 print(f"3 Результат (рекурсия)- {recursion(int(number))}")
-print(timeit.timeit(recursion(int(number))))
-cProfile.run('recursion(int(number))')
+print(timeit.timeit(recursion(int(number)), number=1000))
+cProfile.run('main_recursion()')
+
 
 # 3 Результат (рекурсия)- 987654321
-# 0.005613100000000003
-#          21 function calls (13 primitive calls) in 0.000 seconds
+# 5.499999999998562e-06
+#          18004 function calls (10004 primitive calls) in 0.007 seconds
 #
 #    Ordered by: standard name
 #
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
-#       9/1    0.000    0.000    0.000    0.000 l4_t1.py:35(recursion)
-#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
-#         9    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#         1    0.000    0.000    0.007    0.007 <string>:1(<module>)
+# 9000/1000    0.006    0.000    0.006    0.000 l4_t1.py:45(recursion)
+#         1    0.000    0.000    0.007    0.007 l4_t1.py:52(main_recursion)
+#         1    0.000    0.000    0.007    0.007 {built-in method builtins.exec}
+#      9000    0.000    0.000    0.000    0.000 {built-in method builtins.len}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
--- l4_t2.py
+++ l4_t2.py
@@ -3,50 +3,91 @@
 # Используя алгоритм «Решето Эратосфена»
 # Примечание ко всему домашнему заданию: Проанализировать скорость и сложность алгоритмов.
 # Результаты анализа сохранить в виде комментариев в файле с кодом.
+import cProfile
+import timeit
 
 n = 15
-# print(numbers_list)
 
 
 # Без использования «Решета Эратосфена»;
-def no_eratosthenes(n):
-    numbers_list = [i for i in range(2, n + 1)]
+def no_eratosthenes(number):
     result_list = []
-    for i in numbers_list:
-        count = 0
-        for y in range(2, numbers_list.index(i) - 1):
-            # print(f"i = {i}, y = {y}")
-            if i % y == 0:
-                count += 1
-            else:
-                pass
-        if count == 1:
-            result_list.append(i)
-        else:
-            result_list.append(0)
-
-    return result_list
+    while len(result_list) < number:
+        for i in range(0, number * number):
+            count = 0
+            for y in range(1, i + 1):
+                # print(f"i = {i}, y = {y}")
+                if i % y == 0:
+                    count += 1
+                else:
+                    pass
+            if count == 2:
+                result_list.append(i)
+    return result_list[number - 1]
 
 
-print(no_eratosthenes(n))
+def main_no_eratosthenes():
+    for _ in range(1000):
+        no_eratosthenes(n)
+
+
+print(f'простое число под номером {n} = {no_eratosthenes(n)} (без использования решетки эрастофена')
+print(
+    f'время затраченное на 1000 вызовов без использования решетки эрастофена - {timeit.timeit(f"no_eratosthenes({n})", globals=globals(), number=1000)}')
+print('1000 вызовов для просмотра пролайфера')
+cProfile.run('main_no_eratosthenes()')
+# простое число под номером 15 = 47 (без использования решетки эрастофена
+# время затраченное на 1000 вызовов без использования решетки эрастофена - 0.6509629
+# 1000 вызовов для просмотра пролайфера
+#          51004 function calls in 0.661 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.661    0.661 <string>:1(<module>)
+#      1000    0.658    0.001    0.661    0.001 l4_t2.py:13(no_eratosthenes)
+#         1    0.000    0.000    0.661    0.661 l4_t2.py:29(main_no_eratosthenes)
+#         1    0.000    0.000    0.661    0.661 {built-in method builtins.exec}
+#      2000    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#     48000    0.003    0.000    0.003    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
 
 # Используя алгоритм «Решето Эратосфена»
-def eratosthenes(n):
-    a = []
-    b = []
-    if n < 4:
-        return
-    for i in range(2, n + 1, 1):
-        a.append(i)
-    while a:
-        for i in a[1:]:
-            if i % a[0] == 0:
-                b.append(i)
-                a.remove(i)
-        a = a[1:]
-    for i in a:
-        print(i, end=" ")
+def eratosthenes(number):
+    numbers_list = [i for i in range(2, number * number)]
+
+    for i in numbers_list:
+        for j in numbers_list:
+            if j % i == 0 and i != j:
+                numbers_list.pop(numbers_list.index(j))
+
+    return numbers_list[number - 1]
 
 
-eratosthenes(n)
+def main_eratosthenes():
+    for _ in range(1000):
+        eratosthenes(n)
+
+
+print(f'простое число под номером {n} = {eratosthenes(n)} (с использованием решетки эрастофена)')
+print(
+    f'время затраченное на 1000 вызовов без использования решетки эрастофена - {timeit.timeit(f"eratosthenes({n})", globals=globals(), number=1000)}')
+print('1000 вызовов для просмотра пролайфера')
+cProfile.run('main_eratosthenes()')
+# простое число под номером 15 = 47 (с использованием решетки эрастофена)
+# время затраченное на 1000 вызовов без использования решетки эрастофена - 0.1492388
+# 1000 вызовов для просмотра пролайфера
+#          352004 function calls in 0.199 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.199    0.199 <string>:1(<module>)
+#      1000    0.098    0.000    0.198    0.000 l4_t2.py:41(eratosthenes)
+#      1000    0.004    0.000    0.004    0.000 l4_t2.py:42(<listcomp>)
+#         1    0.000    0.000    0.199    0.199 l4_t2.py:52(main_eratosthenes)
+#         1    0.000    0.000    0.199    0.199 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#    175000    0.078    0.000    0.078    0.000 {method 'index' of 'list' objects}
+#    175000    0.019    0.000    0.019    0.000 {method 'pop' of 'list' objects}
